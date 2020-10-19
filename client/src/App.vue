65386880202c6b96584400974d6d6e88b028132f
Elliot Mackinnon <elliotmackinnon@techtonics-MacBook-Pro.local>
2020-10-19 13:37:29 -0600

    Create Reports and add to routes

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,8 +2,9 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
+      <router-link to="/reports">Reports</router-link>
     </div>
-    <router-view/>
+    <router-view />
   </div>
 </template>
 
---
+++ client/src/Views/Reports.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>Reports</h2>
+</template>
+
+<script>
+export default {
+  name: "Reports",
+};
+</script>
+
+<style scoped>
+</style>
