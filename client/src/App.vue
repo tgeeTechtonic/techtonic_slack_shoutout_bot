65b9b24d43a5b71c2eedf271cee6f27ba18b82df
Brian Roxbury <brian.roxbury@techtonic.com>
2020-10-19 10:16:26 -0600

    Install scss dev dependencies

--- client/src/App.vue
+++ client/src/App.vue
@@ -2,19 +2,20 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
     </div>
     <router-view/>
   </div>
 </template>
 
 <style lang="scss">
+@use './assets/styles/variables.scss' as v;
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: v.$test;
 }
 
 #nav {
