1af505f79b8892373a1cf1dde4b0adecabd0b2b3
Brian Roxbury <brian.roxbury@techtonic.com>
2020-10-26 10:15:39 -0600

    Initial seed data try

--- server/db/seeds/development/shouts.js
+++ server/db/seeds/development/shouts.js
@@ -1,64 +1,62 @@
+const seedData = require('../../mockData/seedData');
+
+const createUser = async (knex, user) => {
+  await knex('users').insert(
+    {
+      email: user.email,
+      first_name: user.first_name,
+      last_name: user.last_name,
+      slack_handle: user.slack_handle,
+      slack_id: user.slack_id,
+    }
+  );
+};
+
+const createValue = async (knex, companyValue) => {
+  await knex('company_values').insert(
+    {
+      value: companyValue.value
+    });
+};
+
+const createShoutout = async (knex, shoutout) => {
+  let shouter = await knex('users').where('id', shoutout.shouter ).first()
+  let shoutee = await knex('users').where('id', shoutout.shoutee).first();
+  let companyValue = await knex('company_values')
+    .where('id', shoutout.company_value)
+    .first()
+    console.log(shouter);
+  await knex('shoutouts').insert({
+    date: shoutout.date, 
+    message: shoutout.message, 
+    channel_name: shoutout.channel_name, 
+    shouter: shouter,
+    shoutee: shoutee,
+    company_value: companyValue.id,
+  });
+};
+    
 exports.seed = async (knex) => {
   try {
     await knex('shoutouts').del();
     await knex('company_values').del();
     await knex('users').del();
-    const valueA = await knex('company_values').insert(
-      {
-        value: 'ugliness',
-      },
-      'id',
-    );
-    const valueB = await knex('company_values').insert(
-      {
-        value: 'prettiness',
-      },
-      'id',
-    );
-    const userA = await knex('users').insert(
-      {
-        email: 'strangerDanger@getawayfrom.me',
-        first_name: 'Grantholemew',
-        last_name: 'Bundles',
-        slack_handle: 'GBundlesInDaHouse',
-        slack_id: 'GA3HASSD3GLN',
-      },
-      'id',
-    );
-    const userB = await knex('users').insert(
-      {
-        email: 'moonshiner@homemadefun.com',
-        first_name: 'Gertrude',
-        last_name: 'Whiskers',
-        slack_handle: 'BringMommyHerBlanket',
-        slack_id: 'CXKJLOKJ4JSHAKD9',
-      },
-      'id',
-    );
-    const shoutoutA = await knex('shoutouts').insert(
-      {
-        date: 'Mon Oct 31 2020',
-        message:
-          "Gertrude put glitter in my coffee this morning and I've been coughing it up for 2 hours",
-        channel_name: 'general',
-        shouter: userA[0],
-        shoutee: userB[0],
-        company_value: valueA[0],
-      },
-      'id',
-    );
-    return knex('shoutouts').insert(
-      {
-        date: 'Tues Nov 4 2020',
-        message: "Mr. Bundles didn't yell at me today",
-        channel_name: 'general',
-        shouter: userB[0],
-        shoutee: userA[0],
-        company_value: valueB[0],
-      },
-      'id',
-    );
+    
+    let userPromises = seedData.users.map(user => {
+      return createUser(knex, user);
+    });
+  
+    let valuePromises = seedData.company_values.map(companyValue => {
+      return createValue(knex, companyValue);
+    });
+
+    let shoutoutPromises = seedData.shoutouts.map(shoutout => {
+      return createShoutout(knex, shoutout);
+    });
+    
+    return Promise.all(shoutoutPromises);
+
   } catch (err) {
     console.error('Error seeding data: ' + err);
-  }
+  };
 };
