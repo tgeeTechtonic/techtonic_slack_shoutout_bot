45e0eea5b1f39dce829414f03ca89bdfa6767a1d
Travis Gee <travisgee@techtonics-MBP.hsd1.co.comcast.net>
2020-10-27 16:31:22 -0600

    Add slack routing to app

--- server/app.js
+++ server/app.js
@@ -1,6 +1,5 @@
-const axios = require('axios');
 const express = require('express');
-const { environment } = require('./dbConfig');
+// const { environment } = require('./dbConfig');
 
 const app = express();
 
@@ -8,5 +7,6 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use('/api', require('./routes/shoutoutRoutes'));
+app.use('/api', require('./routes/slackRoutes'));
 
 module.exports = app;
