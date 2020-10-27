e63887b59da8c131cd5229ccc6a45b314228196d
Elliot Mackinnon <elliotmackinnon@techtonics-MacBook-Pro.local>
2020-10-27 12:00:05 -0600

    Frontend is served by backend with errors

--- server/app.js
+++ server/app.js
@@ -1,8 +1,18 @@
 const axios = require('axios');
 const express = require('express');
+const history = require('connect-history-api-fallback');
 const { environment } = require('./dbConfig');
 
-const app = express();
+let app = express();
+app.use(history());
+const path = require('path').join(__dirname, '../client/dist/')
+app.use(express.static(path));
+
+app.get('/home', (req, res) => {
+  res.sendFile(
+    'index.html', { root: path }
+  );
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
