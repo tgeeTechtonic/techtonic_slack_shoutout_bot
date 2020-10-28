e301e7267b0baf6f3813aa61b2399eed8103abd3
Elliot Mackinnon <elliotmackinnon@techtonics-MacBook-Pro.local>
2020-10-28 09:49:11 -0600

    Uninstall connect-history-api-fallback

--- server/app.js
+++ server/app.js
@@ -1,19 +1,10 @@
 const axios = require('axios');
 const express = require('express');
-const history = require('connect-history-api-fallback');
-const { environment } = require('./dbConfig');
 
 let app = express();
-app.use(history());
 const path = require('path').join(__dirname, '../client/dist/')
+
 app.use(express.static(path));
-
-app.get('/home', (req, res) => {
-  res.sendFile(
-    'index.html', { root: path }
-  );
-});
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
