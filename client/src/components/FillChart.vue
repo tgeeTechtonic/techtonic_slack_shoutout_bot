d1484c171c67ec30eed26bcf4fc49ea1ae306672
Elliot Mackinnon <elliotmackinnon@techtonics-MacBook-Pro.local>
2020-11-02 09:27:00 -0700

    Change stacked and data label values

--- client/src/components/FillChart.vue
+++ client/src/components/FillChart.vue
@@ -1,7 +1,11 @@
 <template>
   <div>
-    <!-- {{ formatShouts(data) }} -->
-    <apexchart :options="options" :series="options.series" />
+    <apexchart
+      height="400"
+      width="800"
+      :options="chartOptions"
+      :series="series"
+    />
   </div>
 </template>
 
@@ -11,10 +15,11 @@ export default {
   props: ["data"],
   data() {
     return {
-      options: {
+      dateRange: 30,
+      chartOptions: {
         chart: {
           type: "area",
-          stacked: false,
+          stacked: true,
           toolbar: {
             autoSelected: "zoom",
             show: true,
@@ -28,24 +33,10 @@ export default {
             opacity: 0.06,
           },
         },
-        colors: ["#00E396", "#0090FF", "#FF5733"],
+        colors: ["#accc20", "#1aa5ca", "#f77046", "#FCFFFD", "#AF125A"],
         dataLabels: {
-          enabled: true,
+          enabled: false,
         },
-        series: [
-          {
-            name: "Total Views",
-            data: this.generateDayWiseTimeSeries(0, 18),
-          },
-          {
-            name: "Unique Views",
-            data: this.generateDayWiseTimeSeries(1, 18),
-          },
-          {
-            name: "Separated Views",
-            data: this.generateDayWiseTimeSeries(2, 18),
-          },
-        ],
         markers: {
           size: 0,
           strokeColor: "#fff",
@@ -106,26 +97,73 @@ export default {
       },
     };
   },
+  computed: {
+    series() {
+      return this.formatSeries(this.data);
+    },
+    initialStartDate() {
+      const startDate = new Date();
+      const pastDate = startDate.getDate() - this.dateRange;
+      startDate.setDate(pastDate);
+      return startDate.toDateString();
+    },
+  },
   methods: {
-    // formatShouts(shouts, filterValue) {
-    //   let formatted = [];
+    formatSeries(data) {
+      // order chronologically
+      // loop over incoming shoutout array and fill an object with values for keys and set to array of 30 zeros
+      // loop over with the dateRange, go through each day and check to see if any shoutouts match that day
+      // if they do, go into formattedSeries object and incrememnt the correspondin element in the correct value array
+      // loop over formattedSeries object and push all arrays into an array of arrays in order for the graph to use
 
-    //   return shouts;
-    // },
-    generateDayWiseTimeSeries(s, count) {
-      var values = [
-        [4, 3, 10, 9, 29, 19, 25, 9, 12, 7, 19, 5, 13, 9, 17, 2, 7, 5],
-        [2, 3, 8, 7, 22, 16, 23, 7, 11, 5, 12, 5, 10, 4, 15, 2, 6, 2],
-        [10, 6, 7, 4, 13, 14, 12, 8, 11, 15, 2, 20, 2, 3, 13, 8, 4, 12],
-      ];
+      let formattedSeries = {};
+
+      const chronological = data
+        .sort((b, a) => a.date - b.date)
+        .slice(0, this.dateRange);
+
+      chronological.forEach((s) => {
+        formattedSeries[s.value]
+          ? null
+          : (formattedSeries[s.value] = new Array(this.dateRange).fill(0));
+      });
+
+      for (let i = this.dateRange; i >= 0; i--) {
+        const date = new Date();
+        const pastDate = date.getDate() - i;
+        date.setDate(pastDate);
+        const dateToCheck = date.toDateString();
+        chronological.forEach((s) => {
+          if (s.date === dateToCheck) {
+            formattedSeries[s.value][this.dateRange - i]++;
+          }
+        });
+      }
+
+      let seriesValues = [];
+
+      for (const value in formattedSeries) {
+        seriesValues.push({
+          name: value,
+          data: this.generateDayWiseTimeSeries(
+            formattedSeries[value],
+            this.dateRange
+          ),
+        });
+      }
+
+      return seriesValues;
+    },
+    generateDayWiseTimeSeries(value, count) {
       var i = 0;
       var series = [];
-      var x = new Date("11 Nov 2012").getTime();
+      var x = new Date(this.initialStartDate.slice(4)).getTime();
       while (i < count) {
-        series.push([x, values[s][i]]);
+        series.push([x, value[i]]);
         x += 86400000;
         i++;
       }
+
       return series;
     },
   },
