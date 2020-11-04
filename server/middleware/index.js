7a215aacacfc6e68a3a33adbb90d4290c97de402
Travis Gee <travisgee@techtonics-MBP.hsd1.co.comcast.net>
2020-11-04 15:18:17 -0700

    Update middleware file to be more dynamic

--- server/middleware/index.js
+++ server/middleware/index.js
@@ -1,48 +1,67 @@
 exports.verifyParams = (req, res, next) => {
   const { path } = req.route;
-  console.log(path);
+
   if (path === '/reports/monthly') verifyMonthlyRankedReport(req, res, next);
   else if (path === '/shoutouts/user/:userId')
-    verifyAllUserShoutouts(req, res, next);
+    veryifyShoutoutsByUser(req, res, next);
   else next();
 };
+
 exports.verifyRequest = (req, res, next) => {
   const { path } = req.route;
-  console.log(path);
+
   if (path === '/update-value') verifyUpdateValuesRequest(req, res, next);
   else next();
 };
 
-verifyAllUserShoutouts = (req, res, next) => {
+checkForMissingParams = (req, requiredParams) => {
+  let missingParams = [];
+  for (param of requiredParams) {
+    if (!Object.keys(req).includes(param)) missingParams.push(param);
+  }
+  return missingParams;
+};
+
+verifyMonthlyRankedReport = (req, res, next) => {
+  const requiredParams = ['month', 'type', 'year'];
+  const { type } = req.query;
+  const missingParams = checkForMissingParams(req.query, requiredParams);
+
+  if (missingParams.length) {
+    return res
+      .status(400)
+      .json({ error: 'Missing query param(s): ' + missingParams });
+  } else if (type !== 'shoutee' && type !== 'shouter') {
+    return res
+      .status(400)
+      .json({ error: 'type should be either shoutee or shouter' });
+  } else next();
+};
+
+veryifyShoutoutsByUser = (req, res, next) => {
   const { userId } = req.params;
   const { type } = req.query;
 
   if (!Number.isInteger(parseInt(userId))) {
-    return res.status(400).json({ error: 'invalid userId' });
+    return res.status(400).json({ error: 'Invalid userId' });
   } else if (!type) {
-    return res.status(400).json({ error: 'missing params' });
+    return res
+      .status(400)
+      .json({ error: 'A type must be provided, (Ex. shoutee or shouter)' });
   } else if (type !== 'shoutee' && type !== 'shouter') {
     return res
       .status(400)
-      .json({ error: 'type should be either shoutee or shouter' });
-  } else next();
-};
-
-verifyMonthlyRankedReport = (req, res, next) => {
-  const { month, type, year } = req.query;
-
-  if (!month || !type || !year) {
-    return res.status(400).json({ error: 'missing params' });
-  } else if (type !== 'shoutee' && type !== 'shouter') {
-    return res
-      .status(400)
-      .json({ error: 'type should be either shoutee or shouter' });
+      .json({ error: 'Type should be either shoutee or shouter' });
   } else next();
 };
 
 verifyUpdateValuesRequest = (req, res, next) => {
-  const { description, value } = req.body;
-  if (!value || !description) {
-    return res.status(400).json({ error: 'value key is missing' });
+  const requiredParams = ['description', 'value'];
+  const missingParams = checkForMissingParams(req.body, requiredParams);
+
+  if (missingParams.length) {
+    return res
+      .status(400)
+      .json({ error: 'Missing request key(s): ' + missingParams });
   } else next();
 };
