6d42faa475bbd8d85c9535c9a9d00f1217576a97
Travis Gee <travisgee@techtonics-MBP.hsd1.co.comcast.net>
2020-11-04 15:20:09 -0700

    Add error handling to users

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -5,7 +5,9 @@ const userController = require('../controllers/userController');
 router.get('/all-users', async (req, res) => {
   try {
     const users = await userController.getAllUsers();
-    res.status(202).json(users);
+
+    if (users.length) res.status(200).json(users);
+    else res.status(204).json('No Content');
   } catch (err) {
     res.status(500).json({ err });
   }
