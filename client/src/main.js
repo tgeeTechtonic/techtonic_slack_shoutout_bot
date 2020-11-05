8b79d1f3e97322a4ddab0bd7efded9ee9d8ffdc2
Travis Gee <travisgee@techtonics-MBP.hsd1.co.comcast.net>
2020-11-05 14:28:32 -0700

    Add normalize file

--- client/src/main.js
+++ client/src/main.js
@@ -3,6 +3,7 @@ import App from './App.vue';
 import router from './router';
 import VueApexCharts from 'vue-apexcharts';
 import Vuetify from 'vuetify/lib';
+import './assets/styles/normalize.css';
 
 Vue.config.productionTip = false;
 Vue.use(Vuetify);
