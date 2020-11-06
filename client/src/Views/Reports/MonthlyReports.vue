2c8f9697aac50a8c33c89cffdd7a666cce4c297f
Travis Gee <travisgee@techtonics-MBP.hsd1.co.comcast.net>
2020-11-06 10:38:48 -0700

    Update table to have loading bar when fetching data

--- client/src/Views/Reports/MonthlyReports.vue
+++ client/src/Views/Reports/MonthlyReports.vue
@@ -1,73 +1,73 @@
 <template>
-    <v-tab-item>
-          <v-date-picker v-model="picker" type="month" color="green darken-1">
-          </v-date-picker>
-          <Table
-            class="reports-container__table"
-            :data="shoutsToDisplay"
-            @toggleView="toggleView"
-            :view="tableView"
-            :dateObj="this.createDateObj()"
+  <v-tab-item>
+    <v-date-picker v-model="picker" type="month" color="green darken-1">
+    </v-date-picker>
+    <Table
+      :data="shoutsToDisplay"
+      :dateObj="this.createDateObj()"
+      class="reports-container__table"
+      :loading="loading"
+      @toggleView="toggleView"
+      :view="tableView"
     />
     <RadarChart :data="shoutsToDisplay.slice(0, 7)" />
-    </v-tab-item>
+  </v-tab-item>
 </template>
 
 <script>
-import { rankedShoutersFormatter, } from "../../shared/formatters";
-import { getRankedByMonth } from "../../shared/apiCalls";
-import Table from "./Table";
-import RadarChart from "./RadarChart";
+import { getRankedByMonth } from '../../shared/apiCalls';
+import { rankedShoutersFormatter } from '../../shared/formatters';
+import RadarChart from './RadarChart';
+import Table from './Table';
 
-    export default {
-      name: "MontlyReports",
-      components: { Table, RadarChart },
-      data () {
-          return {
-              tableView: true,
-              picker: new Date().toISOString().substr(0, 7),
-              shoutsToDisplay: [],
-          }
-      },
-      async created() {
-        const { selectedMonth, selectedYear } = this.createDateObj();
-        const formattedShouts = await getRankedByMonth(
-        "shouter",
+export default {
+  name: 'MontlyReports',
+  components: { Table, RadarChart },
+  data() {
+    return {
+      loading: false,
+      picker: new Date().toISOString().substr(0, 7),
+      shoutsToDisplay: [],
+      tableView: true,
+    };
+  },
+  async created() {
+    this.getRankedList();
+  },
+  methods: {
+    toggleView: function() {
+      this.tableView = !this.tableView;
+    },
+    getRankedList: async function() {
+      this.loading = true;
+      const { selectedMonth, selectedYear } = this.createDateObj();
+      const formattedShouts = await getRankedByMonth(
+        this.tableView ? 'shouter' : 'shoutee',
         selectedMonth,
         selectedYear
-        );
+      );
+      this.loading = false;
+      
+      if (formattedShouts.length) {
         this.shoutsToDisplay = rankedShoutersFormatter(formattedShouts);
-      },
-      methods: {
-        toggleView: function () {
-          this.tableView = !this.tableView;
-        },
-        getRankedList: async function () {
-          const { selectedMonth, selectedYear } = this.createDateObj();
-          const formatedShouts = await getRankedByMonth(
-            this.tableView ? "shouter" : "shoutee",
-            selectedMonth,
-            selectedYear
-        );
-          this.shoutsToDisplay = rankedShoutersFormatter(formatedShouts);
-        },
-        createDateObj: function () {
-          return {
-            selectedMonth: this.picker.split("-")[1],
-            selectedYear: this.picker.split("-")[0],
-          };
+      } else this.shoutsToDisplay = [];
     },
-      },
-      watch: {
-        async picker() {
-        await this.getRankedList();
-        },
-        async tableView() {
-        await this.getRankedList();
-        },
-      },
-  }
+    createDateObj: function() {
+      return {
+        selectedMonth: this.picker.split('-')[1],
+        selectedYear: this.picker.split('-')[0],
+      };
+    },
+  },
+  watch: {
+    async picker() {
+      await this.getRankedList();
+    },
+    async tableView() {
+      await this.getRankedList();
+    },
+  },
+};
 </script>
 
-<style lang="scss">
-</style>
+<style lang="scss"></style>
