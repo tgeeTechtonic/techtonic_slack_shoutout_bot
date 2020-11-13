bebae4fff64ad04f73cd9bc5f76a8ca7b001a8d5
Travis Gee <travisgee@techtonics-MBP.hsd1.co.comcast.net>
2020-11-13 11:05:05 -0700

    Update user controller to send back most used values

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -18,6 +18,9 @@ const createAllUsersRes = async (
   const shoutsGiven = await db('shoutouts').where('shouter', id).count();
   const shoutsReceived = await db('shoutouts').where('shoutee', id).count();
 
+  const mostValueGiven = await getMostUsedCompanyValue(id, 'shouter');
+  const mostValueReceived = await getMostUsedCompanyValue(id, 'shoutee');
+
   return {
     id,
     avatar,
@@ -27,9 +30,30 @@ const createAllUsersRes = async (
     last_name,
     num_shoutouts_given: parseInt(shoutsGiven[0].count),
     num_shoutouts_received: parseInt(shoutsReceived[0].count),
+    most_company_value_given: mostValueGiven,
+    most_company_value_received: mostValueReceived,
     role,
     slack_handle,
   };
+};
+
+const getMostUsedCompanyValue = async (id, type) => {
+  const shoutouts = await db('shoutouts').where(type, id);
+  const allValues = shoutouts.map((shout) => shout.company_value);
+
+  const mostUsedValue = allValues
+    .sort(
+      (a, b) =>
+        allValues.filter((v) => v === a).length -
+        allValues.filter((v) => v === b).length
+    )
+    .pop();
+  const companyValue = await db('company_values').where(
+    'id',
+    mostUsedValue
+  );
+
+  return companyValue[0].value;
 };
 
 module.exports = userController;
