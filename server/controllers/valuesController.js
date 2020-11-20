85e455fb68f2dbb4aed2227e9aaa5f6faf6cab1f
Travis Gee <travisgee@techtonics-MBP.hsd1.co.comcast.net>
2020-11-20 15:39:17 -0700

    Add get all company values endpoint to server

--- server/controllers/valuesController.js
+++ server/controllers/valuesController.js
@@ -1,11 +1,26 @@
 const { db } = require('../dbConfig');
 
 const valuesController = {
+  getAllValues: async () => {
+    return await db('company_values')
+      .select()
+      .then((values) => createRes(values));
+  },
   updateValue: async (companyValue) => {
     return await db('company_values')
       .where({ value: companyValue.value })
       .update({ ...companyValue }, ['id']);
   },
+};
+
+const createRes = (values) => {
+  return values.map(val => {
+    return {
+      active: val.active_status,
+      description: val.description,
+      value: val.value, 
+    }
+  });
 };
 
 module.exports = valuesController;
--- server/routes/valuesRoutes.js
+++ server/routes/valuesRoutes.js
@@ -3,6 +3,17 @@ const router = express.Router();
 const valuesController = require('../controllers/valuesController');
 const { verifyRequest } = require('../middleware');
 
+router.get('/company-values', async (req, res) => {
+  try {
+    const companyValues = await valuesController.getAllValues();
+
+    if (companyValues.length) res.status(200).json(companyValues);
+    else res.status(204).json('No Content');
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+})
+
 router.put('/update-value', verifyRequest, async (req, res) => {
   try {
     const updatedValue = await valuesController.updateValue(req.body);
