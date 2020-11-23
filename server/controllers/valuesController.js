cbcf9b4ffbc17be29a44d9740ad9a13daea96f60
Travis Gee <travisgee@techtonics-MBP.hsd1.co.comcast.net>
2020-11-23 09:44:50 -0700

    Update response to include user data and value totals

--- server/controllers/valuesController.js
+++ server/controllers/valuesController.js
@@ -13,14 +13,42 @@ const valuesController = {
   },
 };
 
-const createRes = (values) => {
-  return values.map(val => {
-    return {
-      active: val.active_status,
-      description: val.description,
-      value: val.value, 
-    }
-  });
+const createRes = async (values) => {
+  let results = [];
+
+  for (let i = 0; i < values.length; i++) {
+    const num_times_used = await db('shoutouts')
+      .where('company_value', values[i].id)
+      .count();
+    const top_shoutee = await getTopUser(values[i].id, 'shoutee');
+    const top_shouter = await getTopUser(values[i].id, 'shouter');
+
+    results.push({
+      active: values[i].active_status,
+      description: values[i].description,
+      num_times_used: num_times_used[0].count,
+      top_user_shoutouts_given: top_shouter,
+      top_user_shoutouts_received: top_shoutee,
+      value: values[i].value,
+    });
+  }
+
+  return results;
+};
+
+const getTopUser = async (valueId, userType) => {
+  const shoutouts = await db('shoutouts').where('company_value', valueId);
+  const userId = shoutouts
+    .map((shout) => shout[userType])
+    .sort(
+      (a, b) =>
+        shoutouts.filter((v) => v === a).length -
+        shoutouts.filter((v) => v === b).length
+    )
+    .pop();
+  const user = await db('users').where('id', userId);
+
+  return user[0].first_name;
 };
 
 module.exports = valuesController;
