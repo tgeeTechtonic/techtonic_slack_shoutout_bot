9dc37082b5cdc0211e1d0d9ec51b37abc7d2395d
Travis Gee <travisgee@techtonics-MBP.hsd1.co.comcast.net>
2020-11-30 15:35:59 -0700

    Update user controller to handle date query

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,25 +1,47 @@
 const { db } = require('../dbConfig');
 
 const userController = {
-  getAllUsers: async () => {
+  getAllUsers: async (start_date, end_date) => {
     return await db('users')
       .select()
       .orderBy('first_name', 'desc')
       .then((users) =>
-        Promise.all(users.map((user) => createAllUsersRes(db, user)))
+        Promise.all(
+          users.map((user) => createAllUsersRes(db, user, start_date, end_date))
+        )
       );
   },
 };
 
 const createAllUsersRes = async (
   db,
-  { id, avatar, email, first_name, job_title, last_name, slack_handle, role }
+  { id, avatar, email, first_name, job_title, last_name, slack_handle, role },
+  start_date,
+  end_date
 ) => {
-  const shoutsGiven = await db('shoutouts').where('shouter', id).count();
-  const shoutsReceived = await db('shoutouts').where('shoutee', id).count();
+  const { startDate, endDate } = getDateRange(start_date, end_date);
 
-  const mostValueGiven = await getMostUsedCompanyValue(id, 'shouter');
-  const mostValueReceived = await getMostUsedCompanyValue(id, 'shoutee');
+  const shoutsGiven = await db('shoutouts')
+    .whereBetween('date', [startDate, endDate])
+    .where('shouter', id)
+    .count();
+  const shoutsReceived = await db('shoutouts')
+    .whereBetween('date', [startDate, endDate])
+    .where('shoutee', id)
+    .count();
+
+  const mostValueGiven = await getMostUsedCompanyValue(
+    id,
+    'shouter',
+    startDate,
+    endDate
+  );
+  const mostValueReceived = await getMostUsedCompanyValue(
+    id,
+    'shoutee',
+    startDate,
+    endDate
+  );
 
   return {
     id,
@@ -37,10 +59,12 @@ const createAllUsersRes = async (
   };
 };
 
-const getMostUsedCompanyValue = async (id, type) => {
-  const shoutouts = await db('shoutouts').where(type, id);
+const getMostUsedCompanyValue = async (id, type, startDate, endDate) => {
+  const shoutouts = await db('shoutouts')
+    .whereBetween('date', [startDate, endDate])
+    .where(type, id);
   if (!shoutouts.length) return '-';
-  
+
   const allValues = shoutouts.map((shout) => shout.company_value);
 
   const mostUsedValue = allValues
@@ -50,12 +74,18 @@ const getMostUsedCompanyValue = async (id, type) => {
         allValues.filter((v) => v === b).length
     )
     .pop();
-  const companyValue = await db('company_values').where(
-    'id',
-    mostUsedValue
-  );
+  const companyValue = await db('company_values').where('id', mostUsedValue);
 
   return companyValue[0].value;
+};
+
+const getDateRange = (start_date, end_date) => {
+  const [year, month] = end_date.split('-');
+  const numDaysInMonth = new Date(year, month, 0).getDate();
+  return {
+    startDate: new Date(start_date),
+    endDate: new Date(year, parseInt(month) - 1, numDaysInMonth),
+  };
 };
 
 module.exports = userController;
