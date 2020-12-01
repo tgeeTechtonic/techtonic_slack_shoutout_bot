7148631d360415c963fcb7b6fcb989cbc8eab6ce
Travis Gee <travisgee@techtonics-MBP.hsd1.co.comcast.net>
2020-12-01 16:22:24 -0700

    Add formatter for single user data

--- client/src/shared/formatters.js
+++ client/src/shared/formatters.js
@@ -23,35 +23,21 @@ export const rankedShoutersFormatter = (shouts) => {
   });
 };
 
-export const userShoutoutsFormatter = (userData) => {
-  const {
-    num_shoutouts_given,
-    num_shoutouts_received,
-    most_company_value_given,
-    most_company_value_received,
-  } = userData.summary;
-
+export const userFormatter = (user) => {
   return {
-    shoutoutsGiven: userData.shoutoutsGiven.map((shout) => ({
+    ...user,
+    shoutoutsGiven: user.shoutoutsGiven.map((shout) => ({
       date: shout.date,
       to: shout.shoutee,
       company_value: shout.company_value,
       message: shout.message,
     })),
-    shoutoutsReceived: userData.shoutoutsReceived.map((shout) => ({
+    shoutoutsReceived: user.shoutoutsReceived.map((shout) => ({
       date: shout.date,
       from: shout.shouter,
       company_value: shout.company_value,
       message: shout.message,
     })),
-    summary: [
-      {
-        total_shoutouts_given: num_shoutouts_given,
-        total_shoutouts_received: num_shoutouts_received,
-        most_company_value_given,
-        most_company_value_received,
-      },
-    ],
   };
 };
 
