09360d6a3e3e4225e92b295e1b2d3146e9adf0ba
Travis Gee <travisgee@techtonics-MBP.hsd1.co.comcast.net>
2020-12-01 16:19:47 -0700

    Add endpoint for getting single user data

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -4,11 +4,26 @@ const userController = require('../controllers/userController');
 
 router.get('/all-users', async (req, res) => {
   try {
-    const start_date = req.query['start-date'];
-    const end_date = req.query['end-date'];
-    const users = await userController.getAllUsers(start_date, end_date);
+    const users = await userController.getAllUsers();
 
     if (users.length) res.status(200).json(users);
+    else res.status(204).json('No Content');
+  } catch (err) {
+    res.status(500).json({ err });
+  }
+});
+
+router.get('/all-users/:userId', async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const start_date = req.query['start-date'];
+    const end_date = req.query['end-date'];
+    const user = await userController.getSingleUser(
+      userId,
+      start_date,
+      end_date
+    );
+    if (user.id) res.status(200).json(user);
     else res.status(204).json('No Content');
   } catch (err) {
     res.status(500).json({ err });
