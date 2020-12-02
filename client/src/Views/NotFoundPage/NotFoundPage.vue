c9c0e2697dbdfef5fb4ecffca9daa268d78c8c24
Desmond Rogers <desmond.rogers@techtonic.com>
2020-12-02 16:45:25 -0600

    refactor: vertically center text

--- client/src/Views/NotFoundPage/NotFoundPage.vue
+++ client/src/Views/NotFoundPage/NotFoundPage.vue
@@ -1,7 +1,7 @@
 <template>
-  <main>
+  <main class="content">
+    <h1 class="not-found-heading">Not Found</h1>
     <section class="not-found-info">
-      <p class="not-found-info__error">404 - Page Not Found</p>
       <p class="not-found-info__subtext">Sorry, we couldn't find the page you were looking for.</p>
       <p class="not-found-info__subtext">¯\_(ツ)_/¯</p>
     </section>
@@ -17,15 +17,24 @@ export default {
 <style lang="scss" scoped>
 @use "@/assets/styles/variables.scss" as v;
 
-.not-found-info {
-  &__error {
-    color: v.$main-white;
-    font-size: 5rem;
-    font-weight: 800;
-  }
+.content {
+  height: 60vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
 
+.not-found-heading {
+  color: v.$main-white;
+  font-size: 5rem;
+  font-weight: 800;
+  font-family: "Fjalla One", "Open Sans", sans-serif;
+}
+
+.not-found-info {
   &__subtext  {
     color: v.$accent-grey;
+    font-size: 1.2rem;
   }
 }
 </style>
