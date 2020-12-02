6f352ff45b63026a445c5b3bbb3b255d04a7a455
Desmond Rogers <desmond.rogers@techtonic.com>
2020-12-02 16:10:21 -0600

    add Not Found page view

---
+++ client/src/Views/NotFoundPage/NotFoundPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <main>
+    <section class="not-found-info">
+      <p class="not-found-info__error">404 - Page Not Found</p>
+      <p class="not-found-info__subtext">Sorry, we couldn't find the page you were looking for.</p>
+      <p class="not-found-info__subtext">¯\_(ツ)_/¯</p>
+    </section>
+  </main>  
+</template>
+
+<script>
+export default {
+  name: 'Not Found',
+}
+</script>
+
+<style lang="scss" scoped>
+@use "@/assets/styles/variables.scss" as v;
+
+.not-found-info {
+  &__error {
+    color: v.$main-white;
+    font-size: 5rem;
+    font-weight: 800;
+  }
+
+  &__subtext  {
+    color: v.$accent-grey;
+  }
+}
+</style>
