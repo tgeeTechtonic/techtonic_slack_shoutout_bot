29a5ad74c6e66ec7db68d5f530e80dcabff6cb97
Desmond Rogers <69487097+drogersTechtonic@users.noreply.github.com>
2020-12-04 12:39:38 -0600

    All shouts Reports view UI enhancements (#143)
    * move the graph to be on top of the table
    
    * add gutters to constrain reports width
    
    * remove id columns
    
    * add scrollbar to table
    
    * place date picker above table
    
    * Update background color for date picker
    
    * Update AllShouts.vue
    
    Co-authored-by: Travis Gee <travisgee@techtonics-MBP.hsd1.co.comcast.net>

--- client/src/Views/ReportsPage/AllShouts.vue
+++ client/src/Views/ReportsPage/AllShouts.vue
@@ -1,34 +1,44 @@
 <template>
-  <v-tab-item>
-    <MonthRangePicker @dateRange="handleDateRange" />
-    <v-dialog v-model="dateRange.invalidDate" width="500">
-      <v-card class="error-card">
-        <v-card-title class="headline error-card__title lighten-2">
-          Invalid Date Range Selected
-        </v-card-title>
-        <v-card-text>
-          The dates you have chosen are not valid, please check your selection
-          and try again.
-        </v-card-text>
-        <v-divider></v-divider>
-        <v-card-actions>
-          <v-spacer></v-spacer>
-          <v-btn color="primary" text @click="dateRange.invalidDate = false">
-            Ok
-          </v-btn>
-        </v-card-actions>
-      </v-card>
-    </v-dialog>
-    <div class="all-reports">
-      <DataTable
-        class="all-reports__table"
-        :data="shoutouts"
-        :loading="loading"
-        :searchable="true"
-        title="All Shoutouts"
-      />
-      <FillChart :data="shoutouts" class="all-reports__chart" />
-    </div>
+  <v-tab-item class="all-container">
+    <v-row>
+      <v-col />
+      <v-col cols="10" xl="6">
+        <div class="all-reports">
+          <FillChart :data="shoutouts" class="all-reports__chart" />
+          <MonthRangePicker @dateRange="handleDateRange" />
+          <v-dialog v-model="dateRange.invalidDate" width="500">
+            <v-card class="error-card">
+              <v-card-title class="headline error-card__title lighten-2">
+                Invalid Date Range Selected
+              </v-card-title>
+              <v-card-text>
+                The dates you have chosen are not valid, please check your
+                selection and try again.
+              </v-card-text>
+              <v-divider></v-divider>
+              <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                  color="primary"
+                  text
+                  @click="dateRange.invalidDate = false"
+                >
+                  Ok
+                </v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
+          <DataTable
+            class="all-reports__table"
+            :data="shoutouts"
+            :loading="loading"
+            :searchable="true"
+            title="All Shoutouts"
+          />
+        </div>
+      </v-col>
+      <v-col />
+    </v-row>
   </v-tab-item>
 </template>
 
@@ -57,7 +67,15 @@ export default {
       return this.$store.state.loading.shoutouts;
     },
     shoutouts() {
-      return this.$store.state.shoutouts;
+      return this.$store.state.shoutouts.map(
+        ({ date, shoutee, shouter, shoutout, value }) => ({
+          date,
+          shoutee,
+          shouter,
+          shoutout,
+          value,
+        })
+      );
     },
   },
   methods: {
@@ -82,6 +100,21 @@ export default {
 <style lang="scss">
 @use "@/assets/styles/variables.scss" as v;
 
+.row-container {
+  background-color: v.$table-white !important;
+  border-radius: 5px 5px 0 0;
+  margin-bottom: 0;
+  width: 100%;
+  height: 90px;
+
+  .col-sm-3,
+  .col-11 {
+    flex: 0 0 33%;
+    max-width: 200px;
+    padding: 20px 0;
+  }
+}
+
 .error-card {
   &__title {
     background-color: v.$accent-blue;
@@ -92,19 +125,40 @@ export default {
   }
 }
 
-.all-reports {
+.all-container {
   background-color: v.$main-bkgrnd;
+}
+
+.all-reports {
   display: flex;
+  flex-direction: column;
+  align-items: center;
   justify-content: space-evenly;
   width: 100%;
 
   &__table {
-    margin: 1rem;
-    width: 60%;
+    width: 100%;
   }
   &__chart {
-    margin: 1rem, 1rem, 0, 1rem;
-    width: 50%;
+    margin: 1rem 1rem 0 1rem;
+    width: 100%;
+  }
+
+  .v-data-table__wrapper {
+    height: 287px;
+    overflow-y: scroll !important;
+
+    &::-webkit-scrollbar {
+      width: 11px;
+    }
+    &::-webkit-scrollbar-track {
+      background: v.$main-white;
+    }
+    &::-webkit-scrollbar-thumb {
+      background-color: v.$main-grey;
+      border-radius: 6px;
+      border: 3px solid v.$main-white;
+    }
   }
 }
 </style>
